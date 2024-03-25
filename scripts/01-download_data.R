e27500b304fb33cab0ee7f7039c25d9bbbf997f6
AhnafAlam1 <ahnafalam18@gmail.com>
2024-03-25 15:38:33 -0400

    uploading data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -9,18 +9,20 @@
 
 
 #### Workspace setup ####
-library(opendatatoronto)
+
 library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+df <- file.choose()
+
+df <- read_csv(df)
 
 
 
 #### Save data ####
 # [...UPDATE THIS...]
 # change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(df, "data/raw_data/df.csv") 
 
          
